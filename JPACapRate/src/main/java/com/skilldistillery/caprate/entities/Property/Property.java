bd20413ba258cc3a5e3a2c7699f8c69c33462eda
Frederick Kaucher <frederick.kaucher@gmail.com>
2022-01-07 15:34:24 -0700

    J Unit test JPACapRate

---
+++ JPACapRate/src/main/java/com/skilldistillery/caprate/entities/Property/Property.java
@@ -0,0 +1,47 @@
+package com.skilldistillery.caprate.entities.Property;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Property {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String address;
+
+	public Property() {
+		super();
+	}
+
+	public Property(int id, String address) {
+		super();
+		this.id = id;
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Property [id=" + id + ", address=" + address + "]";
+	}
+
+}
---
+++ JPACapRate/src/test/java/com/skilldistillery/caprate/entities/Property/PropertyTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.caprate.entities.Property;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PropertyTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Property property;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPACapRate");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		property = em.find(Property.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		property = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(property);
+		assertEquals("1234 Golden Ln", property.getAddress());
+
+	}
+
+}
