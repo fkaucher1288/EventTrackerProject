a0104e15043d23bcbac2096d04c2a6d7b0793344
Frederick Kaucher <frederick.kaucher@gmail.com>
2022-01-09 13:54:58 -0700

    completed all apis except updateProperty

--- CapRateTracker/src/main/java/com/skilldistillery/caprate/controllers/PropertyController.java
+++ CapRateTracker/src/main/java/com/skilldistillery/caprate/controllers/PropertyController.java
@@ -2,8 +2,15 @@ package com.skilldistillery.caprate.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +29,27 @@ public class PropertyController {
 		return propertySvc.getAllProperties();
 	}
 
+	@GetMapping("properties/{id}")
+	public Property getPropertyById(HttpServletResponse response, @PathVariable int id) {
+		Property property = propertySvc.getPropertyById(id);
+		if(property == null) {
+			response.setStatus(404);
+		} return property;
+	}
+	
+	@PutMapping("properties/{id}")
+	public Property updateProperty(@RequestBody Property property, @PathVariable int id) {
+		return propertySvc.updateProperty(property, id);
+	}
+	@DeleteMapping("properties/{id}")
+	public void deleteProperty(@PathVariable int id) {
+		propertySvc.deleteProperty(id);
+	}
+	
+	@PostMapping
+	public Property updateProperty(@RequestBody Property property) {
+		
+	}
 }
+	
+
